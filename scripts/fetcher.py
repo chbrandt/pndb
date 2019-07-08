a61d1a5616c4a089935ad0fa5968d9a450ed47f3
Carlos H Brandt <c.brandt@jacobs-university.de>
2019-07-08 16:46:46 +0200

    Update fetcher parallel/serial functions

--- scripts/fetcher.py
+++ scripts/fetcher.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import sys
 import json
@@ -14,19 +16,15 @@ def get_webpage(url):
 
 def parse_webpage(html):
     from lxml import etree
-    print(html)
     parser = etree.XMLParser()
     root = etree.fromstring(html.replace('&','&amp;'))
     n_tree = root.xpath('//div[@id="datasetlist"][2]')[0]
-    # host = n_tree.xpath('.//div[@id="datasetdetails"]/a')[0].get('href')
     host = '/'.join(_URL.split('/')[:-1])
-    print(host)
     endpoints = []
     for e in n_tree.xpath('.//li'):
         body = e.text.strip()
         uri = e.xpath('./a[2]')[0].get('href')
         url = '/'.join([host, uri])
-        print("Querying", body, url)
         endpoints.append((body, url))
     return endpoints
 
@@ -45,22 +43,21 @@ def pull_data(body, url, outdir):
     geojson = query_wms(body, url)
     filename = os.path.join(outdir, '.'.join([body,'geojson']))
     write_json(geojson, filename)
-    return os.path.isfile(filename)
+    print(body, os.path.isfile(filename))
 
 def run_parallel(endpoints, outdir):
     import multiprocessing as mp
     d = outdir
-
-    output = mp.Queue
-    procs = [mp.Process(target=pull_data, args=(b,u,d) for b,u in endpoints]
+    # output = mp.Queue()
+    procs = [mp.Process(target=pull_data, args=(b,u,d)) for b,u in endpoints]
     for p in procs:
         p.start()
     for p in procs:
         p.join()
-    results = [output.get() for p in procs]
-    return results
+    # results = [output.get() for p in procs]
+    # return results
 
-def main(parallel=False):
+def main(parallel=True):
     page_html = get_webpage(_URL)
     if not page_html:
         print('Deu ruim')
@@ -75,15 +72,14 @@ def main(parallel=False):
         os.mkdir(outdir)
 
     if parallel:
-        results = run_parallel(wms_endpoints, outdir)
+        run_parallel(wms_endpoints, outdir)
     else:
-        results = [pull_data(body, url, outdir) for body,url in wms_endpoints]
-
+        [pull_data(body, url, outdir) for body,url in wms_endpoints]
 
 
 if __name__ == "__main__":
     import sys
-    if sys.argv[1]:
+    if len(sys.argv) > 1:
         msg = "{!s} will download all WMS (GeoJSON) features from {!s}".format(_URL)
         sys.exit(0)
     main()
