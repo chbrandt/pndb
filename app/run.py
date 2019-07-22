6b7ae34b776aae012e5669d393dc7e117ebb024e
Carlos H Brandt <c.brandt@jacobs-university.de>
2019-07-22 16:43:49 +0200

    Implement bodies list, per body centroids list, centroid get

--- app/run.py
+++ app/run.py
@@ -2,32 +2,32 @@ import requests
 from flask import request
 from eve import Eve
 
-# def on_fetched_resource(resource, response):
-#     # del(response['_links'])
-#     del(response['_meta'])
-#     # would result in an empty JSON document
-#     # del(response['_items'])
-#     for doc in response['_items']:
-#         # doc['location'] = doc['location'].get('coordinates')
-#         c = doc['location'].get('coordinates')
-#         doc['lon'] = c[0]
-#         doc['lat'] = c[1]
-#         del(doc['location'])
-#         del(doc['_id'])
-#         del(doc['_created'])
-#         del(doc['_updated'])
-#         del(doc['_etag'])
-#
 app = Eve()
-# app.on_fetched_resource += on_fetched_resource
 
+# @app.route('/centroid/<string:body>/<string:name>')
+# def centroid_by_name(body, name):
+#     url = '?'.join([request.url_root+'centroids',
+#                     'where={"body":"'+body+'","name":"'+name+'"}'])
+#     r = requests.get(url)
+#     return r.json()
 
-@app.route('/centroid/<string:body>/<string:name>')
-def centroid_by_name(body, name):
-    url = '?'.join([request.url_root+'centroids',
-                    'where={"body":"'+body+'","name":"'+name+'"}'])
-    r = requests.get(url)
-    return r.json()
+def on_fetched_resource(resource, response):
+    # del(response['_links'])
+    # del(response['_meta'])
+    for doc in response['_items']:
+        del(doc['_id'])
+        del(doc['_created'])
+        del(doc['_updated'])
+        del(doc['_etag'])
+app.on_fetched_resource += on_fetched_resource
+
+def on_fetched_resource_lonlat(response):
+    for doc in response['_items']:
+        c = doc['location'].get('coordinates')
+        doc['lon'] = c[0]
+        doc['lat'] = c[1]
+        del(doc['location'])
+app.on_fetched_resource_centroid_ll += on_fetched_resource_lonlat
 
 
 if __name__ == '__main__':
--- app/settings.py
+++ app/settings.py
@@ -29,32 +29,51 @@ HATEOAS = False
 # ================================================================
 
 
-centroids = {
-    'item_title': 'centroid',
-    'url': 'centroid/<regex("[\w]+"):body>/<regex("[\w ]+"):name>',
-    'datasource': {
-        'source': 'centroids',
-        'projection': {'name': 1, 'body': 1, 'location.coordinates': 1}
-    }
-}
-
-bodies = {
+bodies_list = {
+    'url': 'centroids/bodies',
     'datasource': {
         'source': 'centroids',
         'aggregation': {
             'pipeline': [
                 {"$group": {"_id": "$body"}},
+                {"$sort": {"_id": 1}},
+                {"$project": {"_id": 0, "body": "$_id"}}
             ]
         },
     }
 }
 
-# body = {
-#     'item_title': 'body craters',
-#     'url': 'centroid/<regex("[\w]+"):body>'
-# }
+centroids_list = {
+    'item_title': 'centroid',
+    'url': 'centroids/<regex("[\w]+"):body>',
+    'datasource': {
+        'source': 'centroids',
+        'projection': {'name': 1, 'body': 1, 'location': 1}
+    }
+}
+
+centroid_get = {
+    'item_title': 'centroid',
+    'url': 'centroids/<regex("[\w]+"):body>/<regex("[\w ]+"):name>',
+    'datasource': {
+        'source': 'centroids',
+        'projection': {'name': 1, 'body': 1, 'location': 1}
+    }
+}
+
+centroid_lonlat_get = {
+    'item_title': 'centroid',
+    'url': 'centroids/<regex("[\w]+"):body>/<regex("[\w ]+"):name>/lonlat',
+    'datasource': {
+        'source': 'centroids',
+        'projection': {'name': 1, 'body': 1, 'location': 1}
+    }
+}
+
 
 DOMAIN = {
-    'centroids': centroids,
-    'bodies': bodies
+    'bodies': bodies_list,
+    'centroids': centroids_list,
+    'centroid': centroid_get,
+    'centroid_ll': centroid_lonlat_get,
 }
